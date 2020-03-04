ab7fbb1460ff1b08dd9935ade26534eecad4cb5d
Shay Lewis <34757626+shaylew@users.noreply.github.com>
2020-03-04 17:35:50 -0500

    tests: add tests for admin-only user route

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -6,28 +6,51 @@ const db = require('../db')
 const app = require('../index')
 const User = db.model('user')
 
-xdescribe('User routes', () => {
+describe.only('User routes', () => {
   beforeEach(() => {
     return db.sync({force: true})
   })
 
-  describe('/api/users/', () => {
-    const codysEmail = 'cody@puppybook.com'
+  describe('admin-only routes', () => {
+    let cody
+    let admin
 
-    beforeEach(() => {
-      return User.create({
+    async function adminLogin() {
+      const agent = request.agent(app)
+      await agent
+        .post('/auth/login')
+        .send({email: 'admin@admin.admin', password: 'password'})
+      return agent
+    }
+
+    const codysEmail = 'cody@puppybook.com'
+    beforeEach(async () => {
+      cody = await User.create({
         name: 'Cody',
         email: codysEmail
       })
+
+      admin = await User.create({
+        name: 'admin',
+        email: 'admin@admin.admin',
+        password: 'password',
+        role: 'admin'
+      })
     })
 
-    it('GET /api/users', async () => {
-      const res = await request(app)
-        .get('/api/users')
-        .expect(200)
+    describe('/api/users/', () => {
+      it("doesn't work for guests", async () => {
+        const res = await request(app)
+          .get('/api/users')
+          .expect(401)
+      })
 
-      expect(res.body).to.be.an('array')
-      expect(res.body[0].email).to.be.equal(codysEmail)
+      it('does work for admins', async () => {
+        const agent = await adminLogin()
+        const res = await agent.get('/api/users').expect(200)
+        expect(res.body).to.be.an('array')
+        expect(res.body[0].email).to.be.equal(codysEmail)
+      })
     })
-  }) // end describe('/api/users')
-}) // end describe('User routes')
+  })
+})
--- server/auth/privileges.js
+++ server/auth/privileges.js
@@ -1,19 +1,17 @@
 const adminsOnly = (req, res, next) => {
-  if (req.user.role !== 'admin') {
-    const err = new Error('Sorry, access denied')
-    err.status = 401
-    return next(err)
+  if (!req.user || req.user.role !== 'admin') {
+    res.sendStatus(401)
+  } else {
+    next()
   }
-  next()
 }
 
 const usersOnly = (req, res, next) => {
   if (!req.user) {
-    const err = new Error('Please login to access')
-    err.status = 401
-    return next(err)
+    res.sendStatus(401)
+  } else {
+    next()
   }
-  next()
 }
 
 module.exports = {adminsOnly, usersOnly}
