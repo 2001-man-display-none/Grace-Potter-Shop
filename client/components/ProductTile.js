f30369c7b51b05434b563a2bcee549f6d722d4d3
Shay Lewis <34757626+shaylew@users.noreply.github.com>
2020-03-04 12:35:36 -0500

    cleanup: simplify redundant Link components

--- client/components/ProductTile.js
+++ client/components/ProductTile.js
@@ -7,11 +7,7 @@ const ProductTile = props => {
       {props.products.map(product => (
         <div key={product.id}>
           <Link to={`/products/${product.id}`}>
-            {' '}
             <img src={product.image} width="200" height="200" />
-          </Link>
-          <Link to={`/products/${product.id}`}>
-            {' '}
             <h2>{product.name}</h2>
           </Link>
           <p>${product.price}</p>
