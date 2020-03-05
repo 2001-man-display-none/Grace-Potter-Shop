742412b169f4cca7a0b8e8419ba3338811a8829b
Shay Lewis <34757626+shaylew@users.noreply.github.com>
2020-03-05 04:26:31 -0500

    cleanup: consolidate react routes into one file

--- client/app.js
+++ client/app.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import {Navbar} from './components'
 import Routes from './routes'
-import {Route} from 'react-router-dom'
-import SingleProductConnect from './pages/SingleProduct'
-import ConnectedProductList from './pages/ProductList'
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Route exact path="/products" component={ConnectedProductList} />
-      <Route
-        exact
-        path="/products/:productId"
-        component={SingleProductConnect}
-      />
       <Routes />
     </div>
   )
