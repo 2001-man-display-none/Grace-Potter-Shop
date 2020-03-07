5ebe4b1d9fc5fa5bc4a8c89e3a49af7b9ecdfddc
eunjoon <joonybejoy@gmail.com>
2020-03-07 18:30:29 -0500

    add:addToCart button on components/productTile.js

--- client/components/ProductTile.js
+++ client/components/ProductTile.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {addToCart} from '../store/cart'
 
 const ProductTile = props => {
   return (
@@ -11,10 +13,24 @@ const ProductTile = props => {
             <h2>{product.name}</h2>
           </Link>
           <p className="price">${product.price}</p>
+          <button
+            id={product.id}
+            type="button"
+            className="button"
+            onClick={() => props.addToCart(product.id)}
+          >
+            Add To Cart
+          </button>
         </div>
       ))}
     </div>
   )
 }
 
-export default ProductTile
+const mapDispatch = dispatch => ({
+  addToCart: productId => dispatch(addToCart(productId))
+})
+
+const ConnectedProductTile = connect(null, mapDispatch)(ProductTile)
+
+export default ConnectedProductTile
