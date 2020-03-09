95baef77edd6a8304c62a01545dc6b5c48569541
Shay Lewis <34757626+shaylew@users.noreply.github.com>
2020-03-09 10:37:54 -0400

    feature: prettify login/signup forms

--- client/components/Signup.js
+++ client/components/Signup.js
@@ -10,29 +10,39 @@ const Signup = props => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit} name="signup">
-        <div>
-          <label htmlFor="name">
-            <small>Name</small>
-          </label>
-          <input name="name" type="text" />
-        </div>
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">Sign Up</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
+      <form
+        className="pure-form pure-form-aligned"
+        onSubmit={handleSubmit}
+        name="signup"
+      >
+        <fieldset>
+          <div className="pure-control-group">
+            <label htmlFor="name">
+              <small>Name</small>
+            </label>
+            <input name="name" type="text" />
+          </div>
+          <div className="pure-control-group">
+            <label htmlFor="email">
+              <small>Email</small>
+            </label>
+            <input name="email" type="text" />
+          </div>
+          <div className="pure-control-group">
+            <label htmlFor="password">
+              <small>Password</small>
+            </label>
+            <input name="password" type="password" />
+          </div>
+          <div className="pure-controls">
+            <button className="pure-button button-primary" type="submit">
+              Sign Up
+            </button>
+          </div>
+          {error && error.response && (
+            <span className="pure-form-message">{error.response.data}</span>
+          )}
+        </fieldset>
       </form>
     </div>
   )
