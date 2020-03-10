5100a0465dae2a0397b2fec40432485677379596
Renata <renata.c.bubic@gmail.com>
2020-03-10 10:56:06 -0400

    working on pagination functionality NOT DONE

--- client/pages/ProductList.js
+++ client/pages/ProductList.js
@@ -2,11 +2,39 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchAll} from '../store/products'
 import ProductTile from '../components/ProductTile'
+import ReactPaginate from 'react-paginate'
 
 export class ProductList extends React.Component {
-  componentDidMount() {
-    this.props.fetchProducts()
+  constructor() {
+    super()
+    this.state = {
+      currPage: 1,
+      productsPerPage: 12,
+      currProducts: [],
+      pageCount: 0
+    }
+    this.handlePageChange = this.handlePageChange.bind(this)
   }
+  async componentDidMount() {
+    await this.props.fetchProducts()
+
+    const state = this.state
+    const iOfLastProduct = state.currPage * state.productsPerPage
+    const iOfFirstProduct = iOfLastProduct - state.productsPerPage
+    this.setState({
+      currProducts: this.props.products.slice(iOfFirstProduct, iOfLastProduct),
+      pageCount: Math.ceil(this.props.products.length / state.productsPerPage)
+    })
+  }
+
+  handlePageChange(pageNumber) {
+    console.log(pageNumber)
+    console.log(typeof pageNumber)
+    this.setState({
+      currPage: Number(pageNumber)
+    })
+  }
+
   render() {
     switch (this.props.status) {
       case 'loading':
@@ -18,9 +46,17 @@ export class ProductList extends React.Component {
           <div className="page products-page">
             <h1>Available Products</h1>
             <div className="product-list">
-              {this.props.products.map(product => (
+              {this.state.currProducts.map(product => (
                 <ProductTile key={product.id} product={product} />
               ))}
+            </div>
+            <div id="pagination">
+              <ReactPaginate
+                activePage={this.state.currPage}
+                pageCount={this.state.pageCount}
+                forcePage={this.state.currPage}
+                onPageChange={this.handlePageChange}
+              />
             </div>
           </div>
         )
