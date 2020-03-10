8841c108611a153ea06b43c20bc2f1ac2a116738
Shay Lewis <34757626+shaylew@users.noreply.github.com>
2020-03-10 02:31:28 -0400

    feature: category model

---
+++ server/db/models/category.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Category = db.define('category', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      is: /[-_a-z0-9]+/
+    },
+    set(slug) {
+      this.setDataValue('slug', slug.toLowerCase())
+    }
+  },
+  inMenu: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: false
+  }
+})
+
+Category.addHook('beforeValidate', category => {
+  if (category.name && !category.slug) {
+    category.slug = category.name.replace(' ', '-')
+  }
+})
+
+Category.findBySlug = function(slug, options = {}) {
+  const mergedOptions = {
+    ...options,
+    where: {
+      ...(options.where || {}),
+      slug: slug.toLowerCase()
+    }
+  }
+  return Category.findOne(mergedOptions)
+}
+
+module.exports = Category
